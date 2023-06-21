5480c9eb5a9eb161a0f0a544975e506098db6846
SaadKoddam <saadkoddam@gmail.com>
2023-06-21 14:19:02 -0700

    Commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,7 +7,6 @@
 
 int main(void)
 {
-
 int i;
 printf("Infinite loop incoming :(\n");
 i = 0;
@@ -15,8 +14,6 @@ i = 0;
 /*{*/
 /*putchar(i);*/
 /*}*/
-
 printf("Infinite loop avoided! \\o/\n");
-
 return (0);
 }
