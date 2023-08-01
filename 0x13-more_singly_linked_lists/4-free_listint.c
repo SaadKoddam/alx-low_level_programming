d1f0d6ebe1218a7d7837057f42da19c0e1d92ec1
SaadKoddam <saadkoddam@gmail.com>
2023-08-01 10:48:38 -0700

    S

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+void free_listint(listint_t *head)
+{
+    listint_t *temp;
+
+    while (head)
+    {
+        temp = head->next;
+        free(head);
+        head = temp;
+    }
+}
